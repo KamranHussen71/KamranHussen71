e1d579509b3e89f6e6b5b5db6fd0947384b88191
KamranHussen71 <140729209+KamranHussen71@users.noreply.github.com>
2023-08-11 23:48:46 +0600

    Add files via upload

---
+++ P5_LiveTest.dart
@@ -0,0 +1,11 @@
+main() {
+  Map<String, dynamic> Persons ={
+    'PersonID': 2122020001,
+    'Name': 'MD Kamran Hussen',
+    'Age': 23,
+    'Salary': 30000,
+    'Gender': 'Male',
+  };
+
+  print(Persons['Salary']);
+}
