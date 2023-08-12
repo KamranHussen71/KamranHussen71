3acb24133f9c04805d3bcfc7c8adc620462ec780
KamranHussen71 <140729209+KamranHussen71@users.noreply.github.com>
2023-08-12 12:20:13 +0600

    Add files via upload

---
+++ P7_M2_Control statements (if, else, for, while).dart
@@ -0,0 +1,14 @@
+void main(){
+  int amount = 1000;
+
+  if(amount >= 100){
+    print('Drink Business');
+  } else if(amount >= 50){
+    print('Cha Business');
+  } else if(amount >= 30){
+    print('Medicine Business');
+  } else {
+    print('Chocolate Business');
+  }
+  print('Continue');
+}
