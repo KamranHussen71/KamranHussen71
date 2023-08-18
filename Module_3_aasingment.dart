23cae2977939878960f66fd3a68f1740d55e444d
KamranHussen71 <140729209+KamranHussen71@users.noreply.github.com>
2023-08-18 23:15:43 +0600

    Update Module_3_aasingment.dart

--- Module_3_aasingment.dart
+++ Module_3_aasingment.dart
@@ -49,11 +49,8 @@ void main() {
   car2.drive(8000);
   car3.drive(15000);
 
-  print("Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles : ${car1.getMilesDriven().round()} Age : ${car1.getAge()}\n");
-  
-  print("Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles : ${car2.getMilesDriven().round()} Age : ${car2.getAge()}\n");
-
-  print("Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles : ${car3.getMilesDriven().round()} Age : ${car3.getAge()}\n");
-
+  print("Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles : ${car1.getMilesDriven().round()} Age : ${car1.getAge()}"); 
+  print("Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles : ${car2.getMilesDriven().round()} Age : ${car2.getAge()}");
+  print("Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles : ${car3.getMilesDriven().round()} Age : ${car3.getAge()}");
   print("Total number of cars created:: ${Car.numberOfCars}");
 }
